b0106401305954b9992ab9cccacc89d62fa23e64
Kevin Janiky <kevin.janiky@gmail.com>
2024-11-28 13:58:17 +0100

    base PHP

---
+++ src/reservation_playgreen.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Plugin Name: Reservation playgreen
+ * Description: Module de reservation sur mesure by AtomikAgency
+ * Version: 1.0.0
+ * Author: AtomikAgency
+ * Author URI: https://atomikagency.fr/
+ */
+
+define('RESERVATION_PLAYGREEN_PLUGIN_DIR', plugin_dir_path(__FILE__));
+define('RESERVATION_PLAYGREEN_PLUGIN_URL', plugin_dir_url(__FILE__));
+
+if (!defined('ABSPATH')) {
+    exit; // Prevent direct access
+}
+
+require_once RESERVATION_PLAYGREEN_PLUGIN_DIR . 'update-checker.php';
