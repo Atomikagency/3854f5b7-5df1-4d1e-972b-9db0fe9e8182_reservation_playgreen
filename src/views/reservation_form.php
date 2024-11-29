f53f3d455a222561a2b6d21f81474d2a67a24533
Clement Menczynski <71343161+clementmenc@users.noreply.github.com>
2024-11-29 17:12:56 +0100

    Validator reservation

--- src/views/reservation_form.php
+++ src/views/reservation_form.php
@@ -4,13 +4,13 @@
         <form id="reservation-form" method="post">
             <?php wp_nonce_field('reservation_form_nonce', 'reservation_form_nonce_field'); ?>
             <!-- Sélection de la date -->
-            <label for="reservation-date">Date</label>
+            <label for="reservation-date">Date *</label>
             <input type="text" id="reservation-date" name="reservation_date" class="flatpickr" placeholder="Choisissez une date" required>
 
             <!-- Sélection du créneau horaire -->
-            <div id="time-slot-container" style="display: none;">
-                <label for="reservation-time">Créneau horaire</label>
-                <select id="reservation-time" name="reservation_time" required>
+            <div id="time-slot-container">
+                <label for="reservation-time">Créneau horaire *</label>
+                <select id="reservation-time" name="reservation_time" required disabled>
                     <option value="">-- Sélectionnez un créneau horaire --</option>
                 </select>
             </div>
@@ -18,20 +18,20 @@
             <!-- Informations personnelles -->
             <div style="display: flex; gap: 10px;">
                 <div style="flex: 1;">
-                    <label for="reservation-nom">Nom</label>
+                    <label for="reservation-nom">Nom *</label>
                     <input type="text" id="reservation-nom" name="reservation_nom" required>
                 </div>
                 <div style="flex: 1;">
-                    <label for="reservation-prenom">Prénom</label>
+                    <label for="reservation-prenom">Prénom *</label>
                     <input type="text" id="reservation-prenom" name="reservation_prenom" required>
                 </div>
             </div>
 
-            <label for="reservation-email">Email</label>
+            <label for="reservation-email">Email *</label>
             <input type="email" id="reservation-email" name="reservation_email" required>
 
             <!-- Langue -->
-            <label for="reservation-langue">Langue</label>
+            <label for="reservation-langue">Langue *</label>
             <select id="reservation-langue" name="reservation_langue" required>
                 <?php if ($activity_meta['langue_fr']) : ?>
                     <option value="fr">Français</option>
@@ -51,9 +51,18 @@
             <label for="reservation-cp">Code promo</label>
             <input type="text" id="reservation-cp" name="reservation_code_promo" >
 
+            <div style="display: grid; grid-template-columns: auto 1fr; align-items: center; gap: 10px;">
+                <input type="checkbox" id="reservation-cgv" name="reservation_cgv" required>
+                <label for="reservation-cgv">
+                    J'accepte que mes données personnelles saisies dans ce formulaire soient utilisées dans le cadre d'une prise de contact. *
+                </label>
+            </div>
+
+            <div id="reservation-error" style="color: red; margin-top: 10px;"></div>
+
 
             <!-- Soumettre -->
-            <button type="submit" class="button reservation-button" name="form_reservation_submit" style="padding: 12px 35px; width: 100%;">Réserver</button>
+            <button type="submit" class="button reservation-button" name="form_reservation_submit" id="reservation-submit" style="padding: 12px 35px; width: 100%;">Réserver</button>
             <p style="margin-top: 20px; color: #000; font-weight: 500;">1: Enfants de 3 à 17 ans - Gratuit pour les moins de 3 ans</p>
         </form>
     </div>
